d84d1474da6aba86731017a76eafd1f1cd3bced1
Hughes <hughestotimee@gmail.com>
2025-09-23 14:40:44 +0000

    Added new Content builders

---
+++ src/api/vehicle-brand/controllers/vehicle-brand.ts
@@ -0,0 +1,7 @@
+/**
+ * vehicle-brand controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::vehicle-brand.vehicle-brand');
---
+++ src/api/vehicle-brand/services/vehicle-brand.ts
@@ -0,0 +1,7 @@
+/**
+ * vehicle-brand service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::vehicle-brand.vehicle-brand');
